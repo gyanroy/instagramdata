c0a32d02e3789333df08b38928c49d0bc8c55d52
aponiar <git.user.raju@aponiar.com>
2020-09-11 11:22:15 +0530

    changes

--- index.js
+++ index.js
@@ -13,34 +13,10 @@ const puppeteer = require('puppeteer');
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
 
     // go to Instagram web profile (this example use Cristiano Ronaldo profile)
-    await page.goto('https://instagram.com/prabhu_india');
-
-    // check username exists or not exists
-    let isUsernameNotFound = await page.evaluate(() => {
-        // check selector exists
-        if(document.getElementsByTagName('h2')[0]) {
-            // check selector text content
-            if(document.getElementsByTagName('h2')[0].textContent == "Sorry, this page isn't available.") {
-                return true;
-            }
-        }
-    });
-
-    if(isUsernameNotFound) {
-        console.log('Account not exists!');
-
-        // close browser
-        await browser.close();
-        return;
-    }
-
-    // get username
-    let userdata = await page.evaluate(() => {      
-
-        if(window["_sharedData"].entry_data.ProfilePage){
-            return window["_sharedData"].entry_data.ProfilePage[0].graphql;
-        }
-        else return window.location.href;
+    await page.goto('https://www.instagram.com/prabhu_india/?__a=1').then(() => {
+        return page.content();
+    }).then((htmlDocument) => {
+        console.log(htmlDocument);
     });
 
     let pageUrl = await page.evaluate(() => {
@@ -48,10 +24,9 @@ const puppeteer = require('puppeteer');
     })
 
     // display the result to console
-    console.log({ userdata,
-                    pageUrl
-            
-                });
+    console.log({ 
+        pageUrl
+    });
 
     // close the browser
     await browser.close();
